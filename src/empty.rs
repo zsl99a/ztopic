f785cfe5dc2adfbf2ecea09412501f8e9ebb8b87
zsl99a <zsl99a@live.com>
2024-04-01 02:28:00 +0800

    优化 empty topic

--- src/empty.rs
+++ src/empty.rs
@@ -1,35 +1,42 @@
-use std::convert::Infallible;
+use std::{convert::Infallible, fmt::Debug};
 
 use futures::{stream::BoxStream, StreamExt};
 
 use crate::{Topic, TopicManager};
 
-pub struct EmptyTopic<T, S> {
+pub struct EmptyTopic<T, L, S> {
+    label: L,
     marker: std::marker::PhantomData<(T, S)>,
 }
 
-impl<T, S> EmptyTopic<T, S> {
-    pub fn new() -> Self {
+impl<T, L, S> EmptyTopic<T, L, S> {
+    pub fn new(label: L) -> Self {
         Self {
+            label,
             marker: std::marker::PhantomData,
         }
     }
 }
 
-impl<T, S> Default for EmptyTopic<T, S> {
+impl<T, S> Default for EmptyTopic<T, &'static str, S> {
     fn default() -> Self {
-        Self::new()
+        Self::new("default")
     }
 }
 
-impl<T, S> Topic<S> for EmptyTopic<T, S>
+impl<T, L, S> Topic<S> for EmptyTopic<T, L, S>
 where
     T: Send + Sync + 'static,
     S: Send + Sync + 'static,
+    L: Debug,
 {
     type Output = T;
 
     type Error = Infallible;
+
+    fn topic(&self) -> String {
+        format!("{:?}", self.label)
+    }
 
     fn init(&self, _: &TopicManager<S>) -> BoxStream<'static, Result<Self::Output, Self::Error>> {
         futures::stream::empty().boxed()
