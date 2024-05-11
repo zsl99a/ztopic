2399cdeae0ae7c4ab5df59d03efc95cea12997cb
Ethereal <zsl99a@live.com>
2024-05-12 05:35:13 +0800

    upgrade axum

--- src/routes.rs
+++ src/routes.rs
@@ -4,7 +4,6 @@ use axum::{
     routing::get,
     Router,
 };
-use tower_http::{compression::CompressionLayer, trace::TraceLayer};
 
 use crate::TopicManager;
 
@@ -12,11 +11,7 @@ pub fn helium_routes<S>(manager: TopicManager<S>) -> Router
 where
     S: Send + Sync + 'static,
 {
-    Router::new()
-        .route("/", get(get_topics))
-        .with_state(manager)
-        .layer(TraceLayer::new_for_http())
-        .layer(CompressionLayer::new())
+    Router::new().route("/", get(get_topics)).with_state(manager)
 }
 
 async fn get_topics<S>(State(manager): State<TopicManager<S>>) -> impl IntoResponse
